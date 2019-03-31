1505fcc03fc9da8bc1933b95e6dd3dda15a21458
zayanit <zayanit@gmail.com>
2019-03-31 19:34:43 +0300

    Symfony Bundles

---
+++ src/Menu/MenuBuilder.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class MenuBuilder
+{
+    private $factory;
+
+    /**
+     * @param FactoryInterface $factory
+     */
+    public function __construct(FactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function createMainMenu(RequestStack $requestStack)
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->addChild('Home', ['route' => 'homepage']);
+        // ... add more children
+
+        return $menu;
+    }
+}
